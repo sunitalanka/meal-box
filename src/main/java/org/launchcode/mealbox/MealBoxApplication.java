e112a826752005127d2bd0532a3a273d688cdda2
Sunita Lanka <sunita.uscareer@gmail.com>
2021-02-24 16:36:39 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/mealbox/MealBoxApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mealbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealBoxApplication.class, args);
+	}
+
+}
